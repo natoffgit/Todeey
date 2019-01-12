6a331baeedae3f9587d50bdf1edf44373723151e
mac admin <admin@macs-mac.local>
2019-01-12 14:44:18 +0700

    Convert to MVC design pattern.

---
+++ Todeey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todeey
+//
+//  Created by mac admin on 6/1/2562 BE.
+//  Copyright © 2562 mac admin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
